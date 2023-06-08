e12a6ac0ee218b37fd06dcfddd30dfeafab1b9f8
viktoriamay <alterkelly@mail.ru>
2023-06-08 05:24:45 +0300

    Refactor styles of Dates component

--- src/components/Dates/Dates.jsx
+++ src/components/Dates/Dates.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './Dates.scss';
